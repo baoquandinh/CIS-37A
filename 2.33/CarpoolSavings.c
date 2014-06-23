9101368cf0def66b8ecb09558f25c3e2c01195e8
Baoquan Dinh <baoquan.dinh@sjsu.edu>
2014-06-23 10:10:30 -0700

    Create 2.33
    Added in 2.33 with correct labels and prompts.

---
+++ 2.33/CarpoolSavings.c
@@ -0,0 +1,29 @@
+/* 
+ * File:   CarpoolSavings.c
+ * Name: Baoquan Dinh
+ * Due Date: June 24, 2014
+ * System Used: Max OSX
+ * Algorithm:
+ * 
+ *
+ * Created on June 23, 2014, 10:01 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    printf("How many miles do you drive to work per day\n");
+    printf("What is the cost to drive per gallon? Enter ($xx.xx)\n");
+    printf("What are the average miles you get per gallon? Enter ($xx.xx)\n");
+    printf("How much are parking fees per day? Enter ($xx.xx)\n");
+    printf("How much are tolls per day? Enter ($xx.xx)\n");
+
+
+    return (EXIT_SUCCESS);
+}
+
