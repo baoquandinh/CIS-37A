a558a2fb601ac93670ebd4e991c8d29176dc15b8
Baoquan Dinh <baoquan.dinh@sjsu.edu>
2014-06-23 11:05:20 -0700

    4.27
    Created project and labels.

---
+++ 4.27/PythagoreanTriples.c
@@ -0,0 +1,21 @@
+/* 
+ * File:   PythagoreanTriples.c
+ * Name: Baoquan Dinh
+ * Due Date: June 24, 2014
+ * System Used: MAC OSX
+ * Algorithm:
+ *
+ * Created on June 23, 2014, 11:04 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
