bb19d989a50d7b6697c088a1016aa8197d6fc8b7
Baoquan Dinh <baoquan.dinh@sjsu.edu>
2014-06-24 09:41:55 -0700

    5.24 + 5.34
    Added in newly created project for 5.24 and 5.34

---
+++ 5.24/5.24.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   5.24.c
+ * Author: baoquandinh
+ *
+ * Created on June 24, 2014, 9:40 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ 5.34/5.34.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   5.34.c
+ * Author: baoquandinh
+ *
+ * Created on June 24, 2014, 9:41 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
