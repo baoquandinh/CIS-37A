432cdf253cb14b2df2aa6c0f7c042f193ddce845
Baoquan Dinh <baoquan.dinh@sjsu.edu>
2014-06-24 10:12:18 -0700

    3.24
    Added in function prototype , for loops, and algorithm.

--- 5.34/5.34.c
+++ 5.34/5.34.c
@@ -3,7 +3,8 @@
  * Name: Baoquan Dinh
  * Due Date: July 1, 2014
  * System Used: Mac OSX
- * Algorithm:
+ * Algorithm: A for loop will loop through specific degrees and call on the
+ * functions to print out the proper results.
  *
  * Created on June 24, 2014, 9:41 AM
  */
@@ -16,6 +17,22 @@
  */
 int main(int argc, char** argv) {
 
+    float celsius(int fahrenheitTemperature); //Returns a Celsius equivalent
+    float fahrenheit(int celsiusTemperature); //Returns a Fahrenheit equivalent
+    
+    //Loop for Celsius
+    for (int i = 0; i <= 100; i++)
+    {
+        
+    }
+    
+    //Loop for Fahrenheit
+    for (int j = 32; j <= 212; j++)
+    {
+        
+    }
+    
+    
     return (EXIT_SUCCESS);
 }
 
