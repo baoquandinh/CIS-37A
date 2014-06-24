8e6e425b616d12937cfcad7dab41a01fa532308d
Baoquan Dinh <baoquan.dinh@sjsu.edu>
2014-06-24 13:05:16 -0700

    Added
    Added in function prototype and heading label

--- 6.14/6.14Main.c
+++ 6.14/6.14Main.c
@@ -1,12 +1,23 @@
 /* 
  * File:   6.14Main.c
- * Author: baoquandinh
+ * Name: Baoquan Dinh
+ * Due Date:
+ * System Used:
+ * Algorithm:
  *
  * Created on June 24, 2014, 12:59 PM
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 99
+
+//Function prototype
+void mean(const unsigned int answer[]); //const means the value will not be changed
+void mean(unsigned int answer[]);
+void mode(unsigned int freq[], unsigned const int answer[]);
+void bubbleSort(int a[]);
+void printArray(unsigned const int a[]);
 
 /*
  * 
