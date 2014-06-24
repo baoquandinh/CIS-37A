539614e3ddfbef4795ba39d1460496871c5cb6be
Baoquan Dinh <baoquan.dinh@sjsu.edu>
2014-06-24 13:13:29 -0700

    Added
    Added in values for the elements in response array

--- 6.14/6.14Main.c
+++ 6.14/6.14Main.c
@@ -24,6 +24,29 @@ void printArray(unsigned const int a[]);
  */
 int main(int argc, char** argv) {
 
+    /*
+    Declare and initialize array name "frequency" to have 10 elements with 
+    value of 0.
+    */
+    unsigned int frequency[10] = {0}; 
+    
+    /*
+     * Declare and initialize array name "response" with size of SIZE and the
+     * value specified
+     */
+    
+    unsigned int response[SIZE] = {6,7,8,9,8,7,8,9,8,9,
+    7,8,9,5,9,8,7,8,7,8,
+    6,7,8,9,3,9,8,7,8,7,
+    7,8,9,8,9,8,9,7,8,9,
+    6,7,8,7,8,7,9,8,9,2,
+    7,8,9,8,9,8,9,7,5,3,
+    5,6,7,2,5,3,9,4,6,4,
+    7,8,9,6,8,7,8,9,7,8,
+    7,4,4,2,5,3,8,7,5,6,
+    4,5,6,1,6,5,7,8,7};
+    
+    
     return (EXIT_SUCCESS);
 }
 
