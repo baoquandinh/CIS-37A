a43f1c2cf3089ebd450cf2b81478096698004bef
Baoquan Dinh <baoquan.dinh@sjsu.edu>
2014-07-01 10:07:21 -0700

    Added in new definition for 6.19
    Added in how to roll dice and print it out.

--- 6.19/DiceRolling.c
+++ 6.19/DiceRolling.c
@@ -7,11 +7,34 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#define SIZE 7
+
 
 /*
  * 
  */
 int main(int argc, char** argv) {
+    
+
+    size_t face;
+    unsigned int roll;
+    unsigned int frequency[SIZE] = {0};
+    
+    srand( time(NULL)); //seed random number generator
+    
+    //Roll dice 60000 time
+    for (roll = 1; roll <= 36000; roll++)
+    {
+        face =  1 + rand()% 6;
+        ++frequency[face];
+    }
+    
+    for ( face = 1; face < SIZE; ++face ) {
+        printf( "%4u%17d\n", face, frequency[ face ] );
+    }
+    
+    printf( "%s%17s\n", "Face", "Frequency" );
 
     return (EXIT_SUCCESS);
 }
