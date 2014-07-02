50e072bd3274137513db61122bfdebb44f525c50
Baoquan Dinh <baoquan.dinh@sjsu.edu>
2014-07-02 11:09:07 -0700

    Added in stuff

--- 6.19/DiceRolling.c
+++ 6.19/DiceRolling.c
@@ -15,15 +15,26 @@
  * 
  */
 int main(int argc, char** argv) {
-    
-
-    size_t face;
-    unsigned int roll1;
-    unsigned int roll2;
-    unsigned int frequency1[SIZE] = {0};
-    unsigned int frequency2[SIZE] = {0};
-    
    
+    size_t face;
+    unsigned int roll;
+    unsigned int frequency[SIZE] = {0};
+    
+    srand(time(NULL)); //Random number generator
+    
+    for (roll = 0; roll <6000000; roll++)
+    {
+        face = 1 + rand() % 6;
+        ++frequency[ face ];
+    }
+    
+    printf("%4s%17s\n", "Face","Frequency");
+    
+    for (face = 1; face  < SIZE; face++)
+    {
+        printf("%4d %17d\n", face, frequency[face]);
+    }
+    
     return (EXIT_SUCCESS);
 }
 
--- fig06_03/main.c
+++ fig06_03/main.c
@@ -8,26 +8,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SIZE 10
+
 /*
  * 
  */ 
 int main(int argc, char** argv) {
     
-    int n[10]; // Array name is 'n' and contains 10 integers
+    int n[] = {4, 2 ,4 ,1 ,4 ,5,3};
     size_t i; //counter
+    int total = 0;
     
-    //initialize elements of array n to 0
-    for(i = 0; i < 10 ; i++)
-    {
-        n[i] = 0; //Set elements at i = 0
-    }
     
     printf("%s%13s\n", "Element", "Value");
     
-    for (i = 0; i < 10; ++i)
+    for (i = 0; i < SIZE; ++i)
     {
         printf("%7lu%13d\n",i ,n[i]);
     }
+    
+     //Find the sum of the array
+    for (i = 0; i < SIZE; i++)
+    {
+        total = total + n[i];
+    }
+    
+    printf("The total sum in the array is %d", total);
+   
 
     return (EXIT_SUCCESS);
 }
