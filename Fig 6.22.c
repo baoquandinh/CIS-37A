ddd9bf22c6b743c3cf4a03826e683815f6e6e0a9
Baoquan Dinh <baoquan.dinh@sjsu.edu>
2014-07-03 13:29:24 -0700

    Created Fig 6.22
    Practice creating and calling functions that deals with multi-d arrays.

---
+++ Fig 6.22.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#define STUDENTS 3
+#define EXAMS 4
+
+//Function prototypes
+int minimum(int grades[][EXAMS], size_t teens, size_t tests);
+int maximum(int grades[][EXAMS], size_t teens, size_t tests);
+double average(const int setOfGrades[], size_t tests);
+void printArray(int grades[][EXAMS], size_t teens, size_t tests);
+
+int main (void)
+{
+	size_t student ; //Student counter
+
+	//Initialize multi-d array for 3 students
+	int studentGrades[STUDENTS][EXAMS] = 
+	{ {77,68,86,73},
+	  {96,87,89,78},
+	  {79,90,86,81}};
+
+	  //Output array studentGrades
+	  puts("The array is:" );
+	  printArray(studentGrades,STUDENTS,EXAMS);
+
+	  //Detemine smallest and largest grade values
+
+	  printf("\n\nLowest grade: %d\nHighest grade: %d\n", 
+	  	minimum(studentGrades,STUDENTS,EXAMS)
+	  	maximum(studentGrades,STUDENTS,EXAMS));
+
+	  //Calculate average for each student
+	  for (student = 0; student < STUDENTS; student++)
+	  {
+	  	printf("The average grade for student %u is %.2f\n",student,average(studentGrades[student],EXAMS) );
+	  }
+}
+
+void printArray(int grades[][EXAMS], size_t teens, size_t tests)
+{
+	size_t i;
+	size_t j;
+
+	for (i = 0; i < teens; i++)
+	{
+		printf("\nstudentGrades[%lu] ", i);
+		for (j = 0; j< tests; j++)
+		{
+			printf("%-5d", grades[i][j]);
+		}
+	}
+}
+
+int minimum(int grades[][EXAMS], size_t teens, size_t tests)
+//int maximum(int grades[][EXAMS], size_t teens, size_t tests)
+
+
