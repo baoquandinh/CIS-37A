0e59cfe316bd749fc109f86727401f4d5bdef24c
Baoquan Dinh <baoquan.dinh@sjsu.edu>
2014-07-07 07:30:26 -0700

    Added in function definition and function itself.

--- 5.39/5.39.c
+++ 5.39/5.39.c
@@ -5,3 +5,35 @@ Due Date: July 6, 2014
 System Used: Mac OSX
 Algorithm
 */
+
+#include <stdio.h>
+
+
+//Functino prototype
+int gcd(int x, int y);
+
+int main (void)
+{
+	int x,y;
+
+	printf("Please enter an integer: ");
+	scanf("%d", &x);
+	printf("Please enter a second integer: ");
+	scanf("%d", &y);
+
+	gcd(x,y);
+}
+
+//Function definition for gcd
+int gcd	(int x, int y)
+{
+	if ( y == 0)
+	{
+		return x;
+	}
+
+	else
+	{
+		return (gcd(y, x % y));
+	}
+}
