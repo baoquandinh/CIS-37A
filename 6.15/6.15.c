a34a77b27e8c150bc014b0a1a3559d662dd66f7b
Baoquan Dinh <baoquan.dinh@sjsu.edu>
2014-07-07 07:53:19 -0700

    Added in function definition, comments, and checks

--- 6.15/6.15.c
+++ 6.15/6.15.c
@@ -6,3 +6,38 @@ System Used: Mac OSX
 Algorithm: 
 */
 
+#include <stdio.h>
+#define SIZE 20
+
+int main (void)
+{
+	size_t i;
+	unsigned int a[SIZE] = {};
+
+	printf("Please enter in 20 numbers from 10-100:\n");
+
+	
+		for (i = 1; i <= SIZE; i++)
+		{
+			printf("%lu)", i);
+			scanf("%d", &a[i]);
+
+			//Check if the value enter is inside the range
+			if(a[i] > 100 || a[i] < 10)
+			{
+				i = 21;
+				puts(""); //Line break
+				printf("Sorry, the number you enter was outside of the designated range\n");
+			}
+			
+		}
+		
+		//Print out the array, if number entered is larger than field, array will print out 0 for remaining
+		puts("Here is your printed array");
+		for (i = 1; i <= SIZE; i++)
+		{
+			printf("%d\n", a[i]);
+		}
+	
+	
+}
