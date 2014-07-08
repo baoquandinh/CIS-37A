fad0d1e69e909ae641314088306838236d94bb60
Baoquan Dinh <baoquan.dinh@sjsu.edu>
2014-07-08 16:15:45 -0700

    Added in path ways to travel through the maze

--- 7.22/7.22.c
+++ 7.22/7.22.c
@@ -6,13 +6,20 @@ Algorithm: Move through the maze till the exit in direction of the right wall.
 */
 
 #include <stdio.h>
+#include <ctype.h>
 #define SIZE 12
+int row;
+int column;
+int tempR;
+int tempC;
 
 int main (void)
 {
-	size_t rows;
-	size_t columns;
+	size_t rows = 0;
+	size_t columns = 0;
+	char key;
 
+	puts("");//Line break
 	char maze[SIZE][SIZE] = {
 		'#','#','#','#','#','#','#','#','#','#','#','#',
 		'#','.','.','.','#','.','.','.','.','.','.','#',
@@ -26,16 +33,134 @@ int main (void)
 		'#','#','#','#','#','#','.','#','#','#','.','#',
 		'#','.','.','.','.','.','.','#','.','.','.','#',
 		'#','#','#','#','#','#','#','#','#','#','#','#'
-	};
+};
 
-	for (rows = 0; rows < SIZE; rows++)
-	{
-		
+//Print out the original maze
+puts("Here is the original array");
+for (rows = 0; rows < SIZE; rows++)
+	{		
 		for (columns = 0; columns < SIZE; columns++)
 		{
-			printf("%c ", maze[rows][columns]);
+			printf("%3c", maze[rows][columns]);
 		}
 		puts("");
+	}
+	//Start of the maze, prints out the first spot
+puts("");
+puts("> is where you start");
+maze[2][0] = '>';
+for (rows = 0; rows < SIZE; rows++)
+	{		
+		for (columns = 0; columns < SIZE; columns++)
+			{
+				printf("%3c", maze[rows][columns]);
+			
+			}
+		puts("");
+	}
+tempR = 2;
+tempC = 0;
+
+puts("Key Legend:");
+puts("w = up");
+puts("a = left");
+puts("s = down");
+puts("d = right");
+
+while (maze[row][column] != maze[4][11])
+	{ 
+		row = tempR;
+		column = tempC;
+		char key = getchar();
+		//printf("%d",key);
+	if (key == 119) // w
+	{
+		row--;
+		maze[row][column] = '^';
+		printf("%d%d\n", row, column);
+		tempR = row;
+		tempC = column;
+		for (row = 0; row < SIZE; row++)
+		{		
+			for (column = 0; column < SIZE; column++)
+			{
+				printf("%3c", maze[row][column]);
+				
+			}
+			puts("");
+
+		}
+	}
+	if (key == 97) //a
+	{
+		column--;
+		maze[row][column] = '<';
+		printf("%d%d\n", row, column);
+		tempR = row;
+		tempC = column;
+		for (row = 0; row < SIZE; row++)
+		{		
+			for (column = 0; column < SIZE; column++)
+			{
+				printf("%3c", maze[row][column]);
+				
+			}
+			puts("");
+
+		}
+	}
+	if (key == 115) //s
+	{
+		row++;
+		maze[row][column] = 'v';
+		printf("%d%d\n", row, column);
+		tempR = row;
+		tempC = column;
+		for (row = 0; row < SIZE; row++)
+		{		
+			for (column = 0; column < SIZE; column++)
+			{
+				printf("%3c", maze[row][column]);
+				
+			}
+			puts("");
+
+		}
+	}
+	if (key == 100) //d
+	{
+		column++;
+		maze[row][column] = '>';
+		printf("%d%d\n", row, column);
+		tempR = row;
+		tempC = column;
+		for (row = 0; row < SIZE; row++)
+		{		
+			for (column = 0; column < SIZE; column++)
+			{
+				printf("%3c", maze[row][column]);
+				
+			}
+			puts("");
+
+		}
 
 	}
+	}
+
+	/*
+	puts("");
+	puts("Congrates, you have reached the end");
+	maze[4][11] = '>';
+	for (rows = 0; rows < SIZE; rows++)
+	{		
+		for (columns = 0; columns < SIZE; columns++)
+		{
+			printf("%3c", maze[rows][columns]);
+			
+		}
+		puts("");
+	}
+
+	*/
 }
