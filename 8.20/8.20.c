b19c5f21b24c821d08ceb6b49320170c00eb0fd4
Baoquan Dinh <baoquan.dinh@sjsu.edu>
2014-07-09 11:19:18 -0700

    Added in function definition, strings, etc.

---
+++ 8.20/8.20.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (void)
+{
+	puts("Here are some inputed sentences and strings");
+
+	const char *s1 = "Hey look, I am writing a sentence with a lot of words!";
+	const char *s2 = "Oh no, I have so so so so so so so so so so so so much words.";
+	const char *s3 = " Hey look I have little words";
+}
