e9c3868f0b4c730f07de54c960941f6ea80fdc7e
Baoquan Dinh <baoquan.dinh@sjsu.edu>
2014-07-10 10:27:36 -0700

    8.16 (Created and Updated)
    Added in code to accept input and print them out.

--- 8.16/8.16.c
+++ 8.16/8.16.c
@@ -3,3 +3,30 @@ Name: Baoquan Dinh
 Due Date: July 10, 2014
 System Used: Max OSX
 */
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#define SIZE 100
+
+int main (void)
+{
+	size_t i;
+	char sentence[SIZE] = {};
+
+	//Accepts input of user
+	puts("Enter some text below:");
+	fgets(sentence, SIZE, stdin);
+
+
+	//Prints out user input to check
+	for (i = 0; i <= strlen(sentence); i++)
+	{
+		if(isalpha(sentence[i]) || isblank(sentence[i]) || ispunct(sentence[i]))
+		{
+		printf("%c", sentence[i] );
+		}
+	}
+
+	puts("");
+}
