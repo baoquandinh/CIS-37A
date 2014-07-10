5285c270530f905577f5b4321ec6b8c11a5dce24
Baoquan Dinh <baoquan.dinh@sjsu.edu>
2014-07-10 12:59:18 -0700

    9.9 Update
    Added in header file and variables.

--- 9.9/9.9.c
+++ 9.9/9.9.c
@@ -4,5 +4,12 @@ Due Date: July 10, 2014
 System Used: Max OSX
 */
 
+#include <stdio.h>
+
 int main (void)
-{}
+{
+	float number = 100.453627;
+
+	printf("Here is the given number: ");
+	printf("%f\n", number);
+}
