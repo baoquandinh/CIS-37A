301b7854001aaf8927c8e73aa1f1c2813caa338e
Baoquan Dinh <baoquan.dinh@sjsu.edu>
2014-07-10 13:04:39 -0700

    9.9 Updated
    Added in print statements to round the number to where specified.

--- 9.9/9.9.c
+++ 9.9/9.9.c
@@ -12,4 +12,20 @@ int main (void)
 
 	printf("Here is the given number: ");
 	printf("%f\n", number);
+	puts("");
+	printf("Rounded to nearest digith: %.0f\n",number);
+	puts("");
+	printf("Rounded to nearest tenth: %.1f\n",number);
+	puts("");
+	printf("Rounded to nearest hundredth: %.2f\n",number);
+	puts("");
+	printf("Rounded to nearest thousandth: %.3f\n",number);
+	puts("");
+	printf("Rounded to nearest ten-thousandth: %.4f\n",number);
 }
+
+
+/*
+Write a program that prints the value 100.453627
+rounded to the nearest digit, tenth, hundredth, thousandth and ten-thousandth.
+*/
