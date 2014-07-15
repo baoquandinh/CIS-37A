23326a2d7d81a5a2f512a2d55510819e22f1872b
Baoquan Dinh <baoquan.dinh@sjsu.edu>
2014-07-15 13:16:50 -0700

    10.12 Update (In Progress)
    Added in heading and main function

---
+++ 10.12/10.12.c
@@ -0,0 +1,16 @@
+/* 
+Name: Baoquan Dinh
+Due Date:
+System Used: Mac OSX
+Algorithm:
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <limits.h>
+
+int main (void)
+{
+	return (EXIT_SUCCESS);
+}
