49c72bc11898cda1283743d63bc5a679df905377
Baoquan Dinh <baoquan.dinh@sjsu.edu>
2014-07-15 14:43:00 -0700

    9.17 Update (Complete)
    Added in counter to increment by 1 and added in all 9 precisions of
    9876.12345

--- 9.17/9.17.c
+++ 9.17/9.17.c
@@ -9,5 +9,15 @@ Algorithm:
 
 int main (void)
 {
-	printf("%.9g\n", 9876.12345);
+	int counter = 1;
+
+	printf("%d) %.1g\n",counter++, 9876.12345);
+	printf("%d) %.2g\n",counter++, 9876.12345);
+	printf("%d) %.3g\n",counter++, 9876.12345);
+	printf("%d) %.4g\n",counter++, 9876.12345);
+	printf("%d) %.5g\n",counter++, 9876.12345);
+	printf("%d) %.6g\n",counter++, 9876.12345);
+	printf("%d) %.7g\n",counter++, 9876.12345);
+	printf("%d) %.8g\n",counter++, 9876.12345);
+	printf("%d) %.9g\n",counter++, 9876.12345);
 }
