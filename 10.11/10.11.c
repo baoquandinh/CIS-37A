2ca731bff876f82917f1e0dbe3c0948bdc2ea50b
Baoquan Dinh <baoquan.dinh@sjsu.edu>
2014-07-15 13:07:53 -0700

    10.11 Update (Complete)
    Added in function definition for power2

--- 10.11/10.11.c
+++ 10.11/10.11.c
@@ -7,8 +7,35 @@ Algorithm:
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+int power2(int number, int power);
 
 int main (void)
 {
+	int num,numPow;
+
+	puts("Enter a number");
+	scanf("%d", &num);
+
+	puts("Enter the power");
+	scanf("%d", &numPow);
+
+	power2(num, numPow);
+
 	return (EXIT_SUCCESS);
 }
+
+int power2(int number, int power)
+{
+	return 0;
+}
+
+
+
+/*
+Left shifting an unsigned int by 1 bit is equivalent to multiplying the value by 2. 
+Write function power2 that takes two integer arguments number and pow and calculates
+number * 2pow
+Use the shift operator to calculate the result. Print the values as integers and as bits.
+*/
